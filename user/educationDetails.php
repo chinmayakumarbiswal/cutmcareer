55fc093e60bd453c2b6336fc29e3480a9b02de0f
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2022-12-02 13:53:22 +0530

    footer change

--- user/educationDetails.php
+++ user/educationDetails.php
@@ -352,18 +352,9 @@ if(isset($_POST['addEdu'])){
 
 
             <!-- Footer Start -->
-            <div class="container-fluid pt-4 px-4">
-                <div class="bg-secondary rounded-top p-4">
-                    <div class="row">
-                        <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a href="https://sakhyat.tech/">sakhyat.tech</a>, All Right Reserved.
-                        </div>
-                        <div class="col-12 col-sm-6 text-center text-sm-end">
-                            Designed By <a href="https://chinmayakumarbiswal.in/">Chinmaya Kumar Biswal</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php
+                include_once('../include/footer.php')
+            ?>
             <!-- Footer End -->
         </div>
         <!-- Content End -->
